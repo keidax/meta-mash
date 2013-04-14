87af5cf19fe4e16e9b3578d805080037d2cd45b1
Gabriel Holodak <gthepiper@gmail.com>
2013-04-14 08:21:01 -0400

    force AudioTagger to use ID3v2.4 tags

--- src/code/AudioTaggerUtilities.java
+++ src/code/AudioTaggerUtilities.java
@@ -9,10 +9,12 @@ import org.jaudiotagger.audio.exceptions.CannotReadException;
 import org.jaudiotagger.audio.exceptions.CannotWriteException;
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.audio.mp3.MP3File;
 import org.jaudiotagger.tag.FieldDataInvalidException;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagException;
+import org.jaudiotagger.tag.id3.ID3v24Tag;
 import org.jaudiotagger.tag.images.Artwork;
 import org.jaudiotagger.tag.images.ArtworkFactory;
 
@@ -21,17 +23,17 @@ public class AudioTaggerUtilities {
 
 		try {
 			File testFile = new File("test_in/song_01.mp3");
-			AudioFile f = AudioFileIO.read(testFile);
-			Tag tag = f.getTag();
-			Artwork cover = tag.getFirstArtwork();
+//			AudioFile f = AudioFileIO.read(testFile);
+			MP3File f = new MP3File(testFile);
+//			Tag tag = f.getTag();
+			ID3v24Tag tag = f.getID3v2TagAsv24();
 			Artwork newCover = ArtworkFactory.createArtworkFromFile(new File("test_in/cover_03.jpg"));
 			tag.deleteArtworkField();
 			tag.addField(newCover);
+			System.out.println(tag.toString());
 			f.setTag(tag);
 			f.commit();
 		
-		} catch (CannotReadException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (TagException e) {
@@ -48,18 +50,17 @@ public class AudioTaggerUtilities {
 
 	public static void setFileArtwork(File targetFile, File artworkFile) {
 		try {
-			AudioFile audioFile = AudioFileIO.read(targetFile);
-			Tag tag = audioFile.getTag();
+			MP3File audioFile = new MP3File(targetFile);
+			ID3v24Tag tag = audioFile.getID3v2TagAsv24();
 
 			Artwork cover = ArtworkFactory.createArtworkFromFile(artworkFile);
 			System.out.println("adding artwork of type " + cover.getMimeType()
 					+ " from " + artworkFile + " to " + targetFile);
 			tag.deleteArtworkField();
 			tag.addField(cover);
+			System.out.println(tag.toString());
 			audioFile.setTag(tag);
 			audioFile.commit();
-		} catch (CannotReadException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (TagException e) {
