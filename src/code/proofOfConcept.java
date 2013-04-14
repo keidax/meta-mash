034ce2d9023d1cebd38de5e67c9fbf817c36e332
Throsby Wells <throsbywells@yahoo.com>
2013-04-13 21:23:40 -0400

    getSongData defined

--- src/code/proofOfConcept.java
+++ src/code/proofOfConcept.java
@@ -3,6 +3,8 @@ package code;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 import org.yaml.snakeyaml.Yaml;
 
@@ -16,9 +18,21 @@ public class proofOfConcept {
 			reader = new FileReader(s);
 			Yaml yammy = new Yaml();
 			Object data = yammy.load(reader);
+			method(data);
 			System.out.println(data);
 			System.out.println(data.getClass().toString());
 			System.out.println(yammy.dump(data));
 		} catch (FileNotFoundException e) {}
+	}
+	
+	public static String method(Object data){
+		
+		Set<String> sSet = ((LinkedHashMap) data).keySet();
+		System.out.println(sSet);
+		String sArtist = "artist: "+ ((LinkedHashMap) data).get("artist")+ "\n";
+		String sAlbum = "album: "+ ((LinkedHashMap) data).get("album")+ "\n";
+		String sTitle = "title: "+ ((LinkedHashMap) data).get("title");
+		System.out.println(sArtist + sAlbum + sTitle);
+		return sArtist + sAlbum + sTitle;
 	}
 }
