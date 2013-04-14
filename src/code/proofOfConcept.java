6c95b2e5c844dba831515d7577717f5805a265bc
Throsby Wells <throsbywells@yahoo.com>
2013-04-13 21:43:00 -0400

    added/edited songData getter methods

--- src/code/proofOfConcept.java
+++ src/code/proofOfConcept.java
@@ -18,21 +18,30 @@ public class proofOfConcept {
 			reader = new FileReader(s);
 			Yaml yammy = new Yaml();
 			Object data = yammy.load(reader);
-			method(data);
+			getData(data);
 			System.out.println(data);
 			System.out.println(data.getClass().toString());
 			System.out.println(yammy.dump(data));
 		} catch (FileNotFoundException e) {}
 	}
 	
-	public static String method(Object data){
-		
-		Set<String> sSet = ((LinkedHashMap) data).keySet();
-		System.out.println(sSet);
-		String sArtist = "artist: "+ ((LinkedHashMap) data).get("artist")+ "\n";
-		String sAlbum = "album: "+ ((LinkedHashMap) data).get("album")+ "\n";
-		String sTitle = "title: "+ ((LinkedHashMap) data).get("title");
+	public static String getData(Object data){
+		String sArtist = getArtist(data);
+		String sAlbum = getAlbum(data);
+		String sTitle = getTitle(data);
 		System.out.println(sArtist + sAlbum + sTitle);
 		return sArtist + sAlbum + sTitle;
+	}
+	
+	public static String getArtist(Object data){
+		return "artist: "+ ((LinkedHashMap) data).get("artist");
+	}
+	
+	public static String getAlbum(Object data){
+		return "album: "+ ((LinkedHashMap) data).get("album");
+	}
+	
+	public static String getTitle(Object data){
+		return "title: "+ ((LinkedHashMap) data).get("title");
 	}
 }
